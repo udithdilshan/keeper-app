ad25727e66e3585863e0723d39b782f8a12db955
udithdilshan <45947626+udithdilshan@users.noreply.github.com>
2021-08-21 21:47:54 +0530

    Keeper App Part 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from './Note'
-import notes from '../note'
+import CreateArea from "./CreateArea";
 function App() {
+  const [notes, setNote] = useState([]);
+  function AddNote(note) {
+    setNote(prevNote => [...prevNote, note]);
+  }
+  function DeleteNote(id) {
+    setNote(prevNote => {
+      return prevNote.filter((note, index) => {
+        return index !== id;
+      })
+    })
+  }
   return (
     <div >
       <Header />
-      {notes.map(note => <Note key={note.id} title={note.title} content={note.content} />)}
+      <CreateArea onAdd={AddNote} />
+      {notes.map((note, index) => <Note onDelete={DeleteNote} id={index} key={index} title={note.title} content={note.content} />)}
       <Footer />
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,11 @@
 import { React } from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function Note(props) {
-    return <div className="note">
+    return <div key={props.id} className="note">
         <h1>{props.title}</h1>
         <p>{props.content}</p>
+        <button onClick={()=>{props.onDelete(props.id)}}><DeleteIcon/></button>
     </div>
 }
 
